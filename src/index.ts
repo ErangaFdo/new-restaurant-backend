f1377b6a27c96ca5b32390380d6ff1c90e9626ec
ErangaFernando404 <erangafdo1994@gmail.com>
2026-01-19 11:22:08 +0530

    connect server_port and mondo_db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello")
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+dotenv.config()
+const SERVER_PORT=process.env.SERVER_PORT
+const MONGO_URL=process.env.MONGO_URL as string
+
+
+
+const app = express()
+app.use(express.json())
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("DB connected Successful")
+  })
+  .catch((err) => {
+    console.error(`DB connection fail: ${err}`)
+    process.exit(1)
+  })
+
+app.listen(SERVER_PORT,()=>{
+    console.log("Server is Running 5000")
+})
+
